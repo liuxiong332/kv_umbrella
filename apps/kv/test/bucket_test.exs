ef135e9fc4ff1426451260e793a795212f0812ff
liuxiong332 <liuxiong332@163.com>
2015-12-30 23:28:45 +0800

    Add put and delete method test for Bucket

--- apps/kv/test/bucket_test.exs
+++ apps/kv/test/bucket_test.exs
@@ -1,5 +1,6 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
+  alias KV.Bucket
 
   setup do
     {:ok, bucket} = KV.Bucket.start_link
@@ -7,6 +8,12 @@ defmodule KV.BucketTest do
   end
 
   test "get and put value", %{bucket: bucket} do
-    assert KV.Bucket.get(bucket, "key") == nil
+    assert Bucket.get(bucket, "key") == nil
+
+    Bucket.put(bucket, "key", "value")
+    assert Bucket.get(bucket, "key") == "value"
+
+    Bucket.delete(bucket, "key")
+    assert Bucket.get(bucket, "key") == nil
   end
 end
